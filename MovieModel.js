401a73bc53eaf71dfc8d5b0d65b2f5fdd65efad9
ngocanh1510 <22520073@gm.uit.edu.vn>
2024-10-30 09:00:39 +0700

    Add files via upload

---
+++ MovieModel.js
@@ -0,0 +1,15 @@
+const mongoose = require ('mongoose')
+
+const movieSchema = new mongoose.Schema (
+    {
+    title: {type: String, required:true},
+    description: {type:String, required:true},
+    duration: {type:String, required:true},
+    genre: {type:String, required:true},
+    rating: { type: String, required:true},
+    limit_age: {type:Number, required:true},
+    poster_url: {type:String, required:true},
+    showtimes: {type:String, required: true}
+    }
+);
+const MovieModel = mongoose.model("movies", movieSchema);
