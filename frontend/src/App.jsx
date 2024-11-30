5906b020c03e602bb6917d5afd9a0dbab93a6ae2
VitaMin270124 <22520878@gm.uit.edu.vn>
2024-11-30 22:48:26 +0700

    add css tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,24 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import "./stylesheets/index.css";
 import Index from "./pages";
 import Loading from "./components/loading";
 import PopErr from "./components/PopErr";
 import PopSuc from "./components/PopSuc";
+import Showtime from "./pages/Showtime";
 function App() {
 
   return (
     <>
-    <Loading/>
+    {/* <Loading/>
     <PopErr/>
-    <PopSuc/>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<Index />}/>
-      </Routes>
-    </BrowserRouter>
+    <PopSuc/> */}
+    <Showtime></Showtime>
+    
+    {/* <Routes>
+      <Route path="/" element={<Index />}/>
+      <Route path='/showtime' element={<Showtime />} />
+    </Routes> */}
+    
     </>
   )
 }
