d0fe86045fa0833cbc9aed954ab0e6506f9a8115
KhaMinh <minhkha06122004@gmail.com>
2024-12-16 03:58:49 +0700

    update api showtime

--- backend/routers/movie-router.js
+++ backend/routers/movie-router.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { getAllMovies, getSearchMovie, getMoviesInHomepage, addMovie, editMovie, deleteMovie, getMovieDetails } from "../controllers/movie-controller.js";
-import { getShowtimeByMovieId,addShowtime, editShowtime, deleteShowtime} from "../controllers/showtime-controller.js";
+import { getShowtimeAndTheaterInfo,addShowtime, editShowtime, deleteShowtime} from "../controllers/showtime-controller.js";
 const movieRouter = express.Router();
     movieRouter.get("/",getMoviesInHomepage);
     movieRouter.get("/all",getAllMovies);
@@ -10,7 +10,7 @@ const movieRouter = express.Router();
     movieRouter.delete("/:id", deleteMovie);
     movieRouter.get("/:id", getMovieDetails);
     movieRouter.post("/:id/showtimes",addShowtime);
-    movieRouter.get("/:id/showtimes",getShowtimeByMovieId);
+    movieRouter.get("/:id/showtimes",getShowtimeAndTheaterInfo);
     movieRouter.put("/:id/showtimes/:showtimeId",editShowtime);
     movieRouter.delete("/:id/showtimes/:showtimeId",deleteShowtime);
 export default movieRouter;
