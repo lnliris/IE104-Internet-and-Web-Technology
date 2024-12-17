88a21f863378f96b73dffe78780093c96f6e97be
Anh <22520073@gm.uit.edu.vn>
2024-12-17 15:04:25 +0700

    update promotion

--- backend/controllers/promotion-controller.js
+++ backend/controllers/promotion-controller.js
@@ -3,7 +3,7 @@ import PromotionModel from  "../models/promotion-model.js";
 export const getPromotionInHomepage = async (req, res) => {
     let promotions;
     try {
-        promotions = await PromotionModel.find(4);
+        promotions = await PromotionModel.find().limit(4);
         res.status(200).json(promotions);
     } catch (error) {
         res.status(500).json({ message: "Failed to retrieve promotions", error });
