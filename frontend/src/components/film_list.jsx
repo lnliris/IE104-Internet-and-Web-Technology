398ad6be8ec7c5d47c2bb463fbb4ca2535201e5d
nhpl894 <22520815@gm.uit.edu.vn>
2024-12-17 19:40:25 +0700

    Fix view fimlist, fix auth

--- frontend/src/components/film_list.jsx
+++ frontend/src/components/film_list.jsx
@@ -1,40 +1,51 @@
-import { useNavigate } from "react-router";
-import Product from "./product";
+import { useNavigate } from 'react-router'
+import Product from './product'
 
-function Filmlist(prop){
-    const nav = useNavigate();
+function Filmlist (prop) {
+  const nav = useNavigate()
 
-    var orderFilm   = function(){
-        nav("/order");
-        window.location.reload()
-    }
+  var orderFilm = function () {
+    nav('/order')
+    window.location.reload()
+  }
 
-    return(
-        <section className="mt-50">
-        <div className="center_ul" id="btn_type_film_list">
-            <button className="btn_cus btn-file-time-cate" id="film_now"><p className="text_upper">Phim đang chiếu</p></button>
-            <button className="btn_cus btn-file-time-cate" id="film_futu"><p className="text_upper">Phim sắp chiếu</p></button>
+  return (
+    <section className='mt-50'>
+      <div className='center_ul' id='btn_type_film_list'>
+        <button className='btn_cus btn-file-time-cate' id='film_now'>
+          <p className='text_upper' onClick={() => {nav('/phimdangchieu'); window.location.reload()}}>Phim đang chiếu</p>
+        </button>
+        <button className='btn_cus btn-file-time-cate disable_btn_film_now' id='film_futu'>
+          <p className='text_upper' onClick={() => {nav('/phimsapchieu'); window.location.reload()}}>
+            Phim sắp chiếu
+          </p>
+        </button>
+      </div>
+
+      <div className='center_ul' id='wrap-product-list'>
+        <div className='center_ul' id='product-list'>
+          <div className='wrap-nodata hide'></div>
+          {prop.data
+            ? prop.data.map(d => (
+                <Product
+                  key={d._id}
+                  clickEvent={() => {}}
+                  orderFilm={orderFilm}
+                  id={d._id}
+                  img={d.poster_url}
+                  type={d.genre}
+                  length={d.duration}
+                  name={d.title}
+                />
+              ))
+            : ''}
         </div>
-        <div className="center_ul" id="wrap-product-list">
-            <div className="center_ul" id="product-list">
-            <div className="wrap-nodata hide"></div>
-            {prop.data ? prop.data.map((d) => (
-                    <Product
-                        key={d._id}
-                        clickEvent = {()=>{}}
-                        orderFilm={orderFilm}
-                        id={d._id}
-                        img={d.poster_url}
-                        type={d.genre}
-                        length={d.duration}
-                        name={d.title}
-                    />
-                )) : ""}
-            </div>
-            <button className="btn_cus" id="more_films"><p className="text_upper">Xem thêm</p></button>
-        </div>
-        </section>
-    )
+        <button className='btn_cus' id='more_films'>
+          <p className='text_upper'>Xem thêm</p>
+        </button>
+      </div>
+    </section>
+  )
 }
 
-export default Filmlist;
+export default Filmlist
--- frontend/src/pages/fiml_list.jsx
+++ frontend/src/pages/fiml_list.jsx
@@ -9,7 +9,7 @@ function FilmList(){
             <ContentFilmList/>
             <Footer/>
         </>
-    
+       
     )
 }
 
