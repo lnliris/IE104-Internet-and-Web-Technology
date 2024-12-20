46778b140ea8696c93bc75507e37cf56e60ce55f
lan <lengoclan2501@gmail.com>
2024-12-21 02:43:51 +0700

    update forrget password

--- backend/routers/auth-router.js
+++ backend/routers/auth-router.js
@@ -7,7 +7,7 @@ import isAdmin from "../middlewares/isAdmin.js";
 
 const AccountRouter = express.Router();
 
-AccountRouter.post('/register', upload.single('avatar'), register); // Đăng ký tài khoản
+AccountRouter.post('/register', register); // Đăng ký tài khoản
 AccountRouter.post('/login', loginLimiter, login); // // Route đăng nhập (áp dụng giới hạn đăng nhập)
 AccountRouter.get('/profile', authMiddleware, getProfile)
 AccountRouter.post('/logout', logout);
