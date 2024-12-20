076c1525fd6f630bdc0f315e3677a9aa1da9f91f
lan <lengoclan2501@gmail.com>
2024-12-20 18:48:11 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,9 @@ mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MO
     .catch((e) => console.log(e));
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+
 app.use("/user", userRouter);
 app.use("/movie", movieRouter);
 app.use("/coupon", couponRouter);
